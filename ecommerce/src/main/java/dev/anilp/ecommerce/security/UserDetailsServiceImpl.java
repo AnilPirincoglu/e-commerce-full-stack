b0b1eca88394066ed9fa67a562af8f138fe7c763
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-11-15 01:15:47 +0300

    add UserDetailsServiceImpl and override loadUserByUserName method

---
+++ ecommerce/src/main/java/dev/anilp/ecommerce/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package dev.anilp.ecommerce.security;
+
+import dev.anilp.ecommerce.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}
+
