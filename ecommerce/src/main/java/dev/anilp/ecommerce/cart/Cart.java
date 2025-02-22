d89ecb14edfe08b1330be0ba0824987f54ded0c6
AnilPirincoglu <anl.pirincoglu@gmail.com>
2025-02-22 14:20:23 +0300

    add: create Cart entity with fields for total price and timestamps

---
+++ ecommerce/src/main/java/dev/anilp/ecommerce/cart/Cart.java
@@ -0,0 +1,43 @@
+package dev.anilp.ecommerce.cart;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cart")
+@EntityListeners(AuditingEntityListener.class)
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "total_price")
+    private BigDecimal totalPrice = BigDecimal.ZERO;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(name = "updated_at", insertable = false)
+    private LocalDateTime updatedAt;
+}
